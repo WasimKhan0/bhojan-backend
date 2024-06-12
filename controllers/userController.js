503d44df1680a7cd0440525eecf37a3eebcd08a0
wasimKhan <96934824+WasimKhan0@users.noreply.github.com>
2024-06-12 11:40:25 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
-// import bcrypt from 'bcrypt'
 import validator from 'validator'
 
 
@@ -28,10 +27,7 @@ const loginUser = async (req, res) => {
         if (!user) {
             return res.json({ success: false, message: "User not found" });
         }
-        // const isMatch = await bcrypt.compare(password, user.password);
-        // if (!isMatch) {
-        //     return res.json({ success: false, message: "Invalid Credentials" });
-        // }
+       
         const token = createToken(user._id);
         return res.json({ success: true, token });
 
@@ -57,8 +53,7 @@ const registerUser = async (req, res) => {
         if (password.length < 4) {
             return res.json({ success: false, message: "Please enter strong password" });
         }
-        // const salt = await bcrypt.genSalt(10)
-        // const hashedPassword = await bcrypt.hash(password, salt);
+      
         const newUser = new userModel({
             name: name,
             email: email,
